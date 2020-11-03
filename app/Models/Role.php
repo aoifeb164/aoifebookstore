fb422c0450b55a823c1357e5c94b7ff51d11732a
vagrant <vagrant@homestead>
2020-11-03 10:55:03 +0000

    role based auth part1

---
+++ app/Models/Role.php
@@ -0,0 +1,17 @@
+<?php
+# @Date:   2020-11-03T10:37:06+00:00
+# @Last modified time: 2020-11-03T10:52:49+00:00
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,9 @@
 <?php
+# @Date:   2020-11-03T10:21:46+00:00
+# @Last modified time: 2020-11-03T10:51:15+00:00
+
+
+
 
 namespace App\Models;
 
@@ -40,4 +45,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+public function roles()
+{
+  return $this->belongsToMany('App\Models\Role');
+}
 }
